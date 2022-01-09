dc35cf06aa63d47d9003794feeabb3df6a455d57
oleh <oleh@localhost.localdomain>
2022-01-09 23:12:33 +0200

    file drop added

--- entropy.py
+++ entropy.py
@@ -1,5 +1,6 @@
 import sys
 import math
+import argparse
 
 import numpy as np
 import plotext as tplot
@@ -10,53 +11,67 @@ from matplotlib.figure import Figure
 
 
 class Window(QtWidgets.QMainWindow):
-    fileSize=0
-    entropy=0
-    freqList=[]
 
     def __init__(self):
         super().__init__()
+        self.__fileSize=None
+        self.__entropy=None
+        self.__freqList=[]
+        self.__fileName=''
+        self.__fileName=sys.argv[1]
+
         self.setAcceptDrops(True)
+        self.setWindowTitle(self.__fileName)
         self._main = QtWidgets.QWidget()
         self.setCentralWidget(self._main)
         layout = QtWidgets.QVBoxLayout(self._main)
         self.canvas = FigureCanvas(Figure(figsize=(6, 4), dpi=100))
         layout.addWidget(self.canvas)
-        self.calculate_entropy()
+        self.calculate_entropy(self.__fileName)
         self.create_plot()
         self.canvas.draw()
 
-    def calculate_entropy(self):
-        with open(sys.argv[1], 'rb') as f:
+    def calculate_entropy(self, file):
+        with open(file, 'rb') as f:
             byteArr = list(f.read())
         f.close()
-        self.fileSize = len(byteArr)
+        self.__fileSize = len(byteArr)
         for b in range(256):
-            ctr = 0 
+            ctr = 0
             for byte in byteArr:
                 if byte == b:
                     ctr += 1
-            self.freqList.append(float(ctr) / self.fileSize)
+            self.__freqList.append(float(ctr) / self.__fileSize)
         ent = 0.0
-        for freq in self.freqList:
+        for freq in self.__freqList:
             if freq > 0:
                 ent = ent + freq * math.log(freq, 2)
         ent = -ent
-        self.entropy=ent
+        self.__entropy=ent
 
     def create_plot(self):
-        n=len(self.freqList)
+        n=len(self.__freqList)
         ind=np.arange(n)
         width=1.0
 
         ax = self.canvas.figure.add_subplot(111)
-        ax.bar(ind, self.freqList, width)
+        ax.bar(ind, self.__freqList, width)
 
         ax.set_autoscalex_on(False)
         ax.set_xlim([0,255])
         ax.set_ylabel('Frequency')
         ax.set_xlabel('Byte')
         ax.set_title('Frequency of bytes from 0 to 255')
+
+    def create_cli_plot(self):
+        n=len(self.__freqList)
+        ind=np.arange(n)
+        tplot.bar(ind, self.__freqList)
+        tplot.title("Frequency of bytes from  0 to 255\nFILENAME: " + self.__fileName)
+        tplot.show()
+
+    def set_new_file_name(self, file_name):
+        self.__fileName=file_name
 
     def dragEnterEvent(self, e):
         if e.mimeData().hasUrls:
@@ -72,9 +87,15 @@ class Window(QtWidgets.QMainWindow):
 
     def dropEvent(self, event):
         files = [u.toLocalFile() for u in event.mimeData().urls()]
+        new_file=''
         for f in files:
-            print(f)
-
+            new_file=f
+        self.canvas.figure.clf()
+        self.set_new_file_name(new_file)
+        self.setWindowTitle(new_file)
+        self.calculate_entropy(new_file)
+        self.create_plot()
+        self.canvas.draw()
 
 if __name__ == "__main__":
     qapp = QtWidgets.QApplication(sys.argv)
